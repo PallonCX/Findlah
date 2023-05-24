bc1e110c577a3aa992dfbe1c4d5d79724601644e
Tan Chee Xiang (Pallon) <102943362+PallonCX@users.noreply.github.com>
2023-05-25 06:36:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-(function(window, mapster){
+(function(window, mapster) {
 
     // map options
-    var options = mapster.MAP_OPTIONS,
-    element = document.getElementById("map-canvas"),
+    var options = mapster.MAP_OPTIONS;
+    var element = document.getElementById("map-canvas");
 
     // map
-    map = mapster.create(element, options);
+    var map = mapster.create(element, options);
 
     var geocoder = new google.maps.Geocoder();
 
-    function geocode(opts) {
+    function geocode(opts, geocoder) {
         geocoder.geocode({
             address: opts.address
-        }, function(results, status){
-            if(status === google.maps.GeocoderStatus.OK) {
+        }, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
                 opts.success.call(this, results, status);
             } else {
                 opts.error.call(this, status);
@@ -33,6 +33,6 @@
         error: function(status) {
             console.error(status);
         }
-    });
+    }, geocoder);
 
-}(window, window.Mapster || (window.Mapster = {})))
+})(window, window.Mapster || (window.Mapster = {}));
