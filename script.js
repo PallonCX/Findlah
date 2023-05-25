ca733c5a20c9f5c0f6afb84dec9b598982735fbf
Tan Chee Xiang (Pallon) <102943362+PallonCX@users.noreply.github.com>
2023-05-25 10:10:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
     let map;
     
     function initMap() {
-        map = new google.maps.Map(document.getElementById("map-canvas"), {
+        map = new google.maps.Map(document.getElementById("map"), {
             center: {
-                lat: 0.0,
-                lng: 0.0
+                lat: 33.0,
+                lng: 33.0
             },
             zoom: 8,
         }); 
