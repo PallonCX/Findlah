a06e9fa94216f90f3942100bebeb497ad28c1e41
Tan Chee Xiang (Pallon) <102943362+PallonCX@users.noreply.github.com>
2023-05-25 09:46:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,32 +7,32 @@
     // map
     var map = mapster.create(element, options);
 
-    var geocoder = new google.maps.Geocoder();
+//     var geocoder = new google.maps.Geocoder();
 
-    function geocode(opts, geocoder) {
-        geocoder.geocode({
-            address: opts.address
-        }, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                opts.success.call(this, results, status);
-            } else {
-                opts.error.call(this, status);
-            }
-        });
-    }
+//     function geocode(opts, geocoder) {
+//         geocoder.geocode({
+//             address: opts.address
+//         }, function(results, status) {
+//             if (status === google.maps.GeocoderStatus.OK) {
+//                 opts.success.call(this, results, status);
+//             } else {
+//                 opts.error.call(this, status);
+//             }
+//         });
+//     }
 
-    geocode({
-        address: "",
-        success: function(results) {
-            var result = results[0];
-            map.addMarker({
-                lat: result.geometry.location.lat(),
-                lng: result.geometry.location.lng()
-            });
-        },
-        error: function(status) {
-            console.error(status);
-        }
-    }, geocoder);
+//     geocode({
+//         address: "",
+//         success: function(results) {
+//             var result = results[0];
+//             map.addMarker({
+//                 lat: result.geometry.location.lat(),
+//                 lng: result.geometry.location.lng()
+//             });
+//         },
+//         error: function(status) {
+//             console.error(status);
+//         }
+//     }, geocoder);
 
 })(window, window.Mapster || (window.Mapster = {}));
