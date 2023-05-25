5312d4db34689058c8938d8b9b7c8faff84b229e
Tan Chee Xiang (Pallon) <102943362+PallonCX@users.noreply.github.com>
2023-05-25 11:09:01 +0800

    Update Mapster.js

--- Mapster.js
+++ Mapster.js
@@ -1,105 +1,114 @@
-(function(window, google, List) {
+MAP_OPTIONS = {
+    center: {
+        lat: 1.290270,
+        lng: 103.851959
+    },
+    zoom: 12,
+//         geocoder: true
+};
 
-    var Mapster = (function(){
+// (function(window, google, List) {
 
-        function Mapster(element, opts) {
-            this.gMap = new google.maps.Map(element, opts);
-//             this.markers = List.create();
-//             if(opts.geocoder) {
-//                 this.geocoder = new google.maps.Geocoder();
-//             }
-        }
+//     var Mapster = (function(){
 
-//         Mapster.prototype = {
-//             _on: function(opts) {
-//                 var self = this;
-//                 google.maps.event.addListener(opts.obj, opts.event, function(e){opts.callback.call(self, e);});
-//             },
+//         function Mapster(element, opts) {
+//             this.gMap = new google.maps.Map(element, opts);
+// //             this.markers = List.create();
+// //             if(opts.geocoder) {
+// //                 this.geocoder = new google.maps.Geocoder();
+// //             }
+//         }
 
-//             geocode: function(opts) {
-//                 var geocoder = new google.maps.Geocoder(); // Declare geocoder variable
-//                 geocoder.geocode({
-//                     address: opts.address
-//                 }, function(results, status){
-//                     if(status === google.maps.GeocoderStatus.OK) {
-//                         opts.success.call(this, results, status);
-//                     } else {
-//                         opts.error.call(this, status);
-//                     }
-//                 });
-//             },
+// //         Mapster.prototype = {
+// //             _on: function(opts) {
+// //                 var self = this;
+// //                 google.maps.event.addListener(opts.obj, opts.event, function(e){opts.callback.call(self, e);});
+// //             },
 
-//             addMarkerByLocation: function(opts) {
-//                 var self = this;
-//                 this.geocode({ // Change this.map to this.geocode
-//                     address: opts.location,
-//                     success: function(results) {
-//                         results.forEach(function(result) {
-//                             opts.lat = result.geometry.location.lat();
-//                             opts.lng = result.geometry.location.lng();
-//                             self.addMarker(opts); // Change this.map.addMarker to self.addMarker
-//                         });
-//                     },
-//                     error: function(status) {
-//                         console.error(status);
-//                     }
-//                 });
-//             },
+// //             geocode: function(opts) {
+// //                 var geocoder = new google.maps.Geocoder(); // Declare geocoder variable
+// //                 geocoder.geocode({
+// //                     address: opts.address
+// //                 }, function(results, status){
+// //                     if(status === google.maps.GeocoderStatus.OK) {
+// //                         opts.success.call(this, results, status);
+// //                     } else {
+// //                         opts.error.call(this, status);
+// //                     }
+// //                 });
+// //             },
 
-//             addMarker: function(opts) {
-//                 var marker;
-//                 opts.position = {
-//                     lat: opts.lat,
-//                     lng: opts.lng
-//                 };
-//                 marker = this._createMarker(opts);
-//                 this.markers.add(marker);
-//                 if(opts.event) {
-//                     this._on({
-//                         obj: marker,
-//                         event: opts.event.name,
-//                         callback: opts.event.callback
-//                     });
-//                 }
-//                 if(opts.content){
-//                     this._on({
-//                         obj: marker,
-//                         event: "click",
-//                         callback: function() {
-//                             var infoWindow = new google.maps.InfoWindow({content: opts.content});
-//                             infoWindow.open(this.gMap, marker);
-//                         }
-//                     });
-//                 }
-//                 return marker;              
-//             },
+// //             addMarkerByLocation: function(opts) {
+// //                 var self = this;
+// //                 this.geocode({ // Change this.map to this.geocode
+// //                     address: opts.location,
+// //                     success: function(results) {
+// //                         results.forEach(function(result) {
+// //                             opts.lat = result.geometry.location.lat();
+// //                             opts.lng = result.geometry.location.lng();
+// //                             self.addMarker(opts); // Change this.map.addMarker to self.addMarker
+// //                         });
+// //                     },
+// //                     error: function(status) {
+// //                         console.error(status);
+// //                     }
+// //                 });
+// //             },
 
-//             findBy: function(callback) {
-//                 return this.markers.find(callback);
-//             },
+// //             addMarker: function(opts) {
+// //                 var marker;
+// //                 opts.position = {
+// //                     lat: opts.lat,
+// //                     lng: opts.lng
+// //                 };
+// //                 marker = this._createMarker(opts);
+// //                 this.markers.add(marker);
+// //                 if(opts.event) {
+// //                     this._on({
+// //                         obj: marker,
+// //                         event: opts.event.name,
+// //                         callback: opts.event.callback
+// //                     });
+// //                 }
+// //                 if(opts.content){
+// //                     this._on({
+// //                         obj: marker,
+// //                         event: "click",
+// //                         callback: function() {
+// //                             var infoWindow = new google.maps.InfoWindow({content: opts.content});
+// //                             infoWindow.open(this.gMap, marker);
+// //                         }
+// //                     });
+// //                 }
+// //                 return marker;              
+// //             },
 
-//             removeBy: function(callback) {
-//                 this.markers.find(callback, function(markers) {
-//                     markers.forEach(function(marker) {
-//                         marker.setMap(null);
-//                     });
-//                 });
-//             },
+// //             findBy: function(callback) {
+// //                 return this.markers.find(callback);
+// //             },
 
-//             _createMarker: function(opts) {
-//                 opts.map = this.gMap;
-//                 return new google.maps.Marker(opts);
-//             }
-//         };
+// //             removeBy: function(callback) {
+// //                 this.markers.find(callback, function(markers) {
+// //                     markers.forEach(function(marker) {
+// //                         marker.setMap(null);
+// //                     });
+// //                 });
+// //             },
 
-        return Mapster;
+// //             _createMarker: function(opts) {
+// //                 opts.map = this.gMap;
+// //                 return new google.maps.Marker(opts);
+// //             }
+// //         };
 
-    })();
+//         return Mapster;
 
-    Mapster.create = function(element, opts) {
-        return new Mapster(element, opts);
-    };
+//     })();
 
-    window.Mapster = Mapster;
+//     Mapster.create = function(element, opts) {
+//         return new Mapster(element, opts);
+//     };
 
-}(window, google, List));
+//     window.Mapster = Mapster;
+
+// }(window, google, List));
